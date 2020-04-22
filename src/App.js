6309a304ff6d35e5866fc02d6e936a0d715e1225
Anthony Thuillez <anthony.thuillez@fidesio.com>
2020-04-23 01:59:00 +0200

    refacto and add reset css

--- src/App.js
+++ src/App.js
@@ -1,57 +1,17 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-// App routes
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './pages/home';
 import Projects from './pages/projects';
 import About from './pages/about';
 import Contact from './pages/contact';
 
-// App components
-import Main from './components/layouts/main';
-
 export default function App() {
   return (
-    <Main>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">La casa</Link>
-              </li>
-              <li>
-                <Link to="/projects">Nos casses</Link>
-              </li>
-              <li>
-                <Link to="/about">Sous le masque</Link>
-              </li>
-              <li>
-                <Link to="/contact">Un (r)enseignement ?</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/projects">
-              <Projects />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-            <Route exact path="/contact">
-              <Contact />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </Main>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </Router>
   );
 }
