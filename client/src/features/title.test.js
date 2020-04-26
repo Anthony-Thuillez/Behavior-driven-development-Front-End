0ee5bbbe91bc67a5736abafbaf1941e9f670931c
Simon <soleausimon@hotmail.fr>
2020-04-26 23:13:09 +0200

    [TEST] title

--- client/src/features/title.test.js
+++ client/src/features/title.test.js
@@ -4,39 +4,59 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-styled-components';
 
-import { Medium, Large, MediumRed, LargeRed } from '../stories/title.stories';
+import { Default, TitleMedium, TitleLarge, TitleMediumWithTextMirror, TitleLargeWithTextMirror } from '../stories/title.stories';
+
 
 describe('Renders Title component', () => {
 
     test('with a Medium title state', () => {
-        const { container, getByTestId } = render(<Medium />);
+        const { container, getByTestId } = render(<TitleMedium />);
         expect(container).toBeTruthy();
         const text = getByTestId('medium-title')
         expect(text).toHaveStyleRule('font-size', '93px');
+        expect(text).toHaveStyleRule('color', '#fff');
     });
 
     test('with a Large title state', () => {
-        const { container, getByTestId } = render(<Large />);
+        const { container, getByTestId } = render(<TitleLarge />);
         expect(container).toBeTruthy();
         const text = getByTestId('large-title')
         expect(text).toHaveStyleRule('font-size', '115px');
+        expect(text).toHaveStyleRule('color', '#fff');
     });
 
-    test('with a Large title state', () => {
-        const { container, getByTestId } = render(<MediumRed />);
+    test('with a Default title state', () => {
+        const { container, getByTestId } = render(<Default />);
         expect(container).toBeTruthy();
-        const text = getByTestId('medium-red-title')
+        const text = getByTestId('default-title')
         expect(text).toHaveStyleRule('font-size', '93px');
-        expect(text).toHaveStyleRule('color', 'rgba(183,39,38,0.86)');
+        expect(text).toHaveStyleRule('color', '#fff');
     });
 
-    test('with a LargeRed title state', () => {
-        const { container, getByTestId } = render(<LargeRed />);
+    test('with a Medium title state', () => {
+        const { container, getByTestId } = render(<TitleMediumWithTextMirror />);
         expect(container).toBeTruthy();
-        const text = getByTestId('large-title')
-        expect(text).toHaveStyleRule('font-size', '115px');
-        expect(text).toHaveStyleRule('color', 'rgba(183,39,38,0.86)');
+        const text = getByTestId('medium-textmiror-title')
+        expect(text).toHaveStyleRule('font-size', '93px')
+        expect(text.innerHTML).toMatch('Titre medium miror')
     });
+
+    test('with a Medium title state', () => {
+        const { container, getByTestId } = render(<TitleLargeWithTextMirror />);
+        expect(container).toBeTruthy();
+        const text = getByTestId('large-textmiror-title')
+        expect(text).toHaveStyleRule('font-size', '115px');
+        expect(text.innerHTML).toMatch('Titre large miror')
+    });
+
+
+    // test('with a LargeRed title state', () => {
+    //     const { container, getByTestId } = render(<LargeRed />);
+    //     expect(container).toBeTruthy();
+    //     const text = getByTestId('large-title')
+    //     expect(text).toHaveStyleRule('font-size', '115px');
+    //     expect(text).toHaveStyleRule('color', 'rgba(183,39,38,0.86)');
+    // });
 
 
 });
--- client/src/stories/title.stories.js
+++ client/src/stories/title.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Title from '../components/atoms/title';
 
-export const Default = () => <Title />
-export const TitleMedium = () => <Title text="Titre medium" size="medium" color="#fff" />
-export const TitleLarge = () => <Title text="Titre large" size="large" color="#fff" />
-export const TitleMediumWithTextMirror = () => <Title text="Titre medium" size="medium" color="#fff" textMirror="Titre medium" />
-export const TitleLargeWithTextMirror = () => <Title text="Titre large" size="large" color="#fff" textMirror="Titre large" />
+export const Default = () => <Title testid="default-title" />
+export const TitleMedium = () => <Title testid="medium-title" text="Titre medium" size="medium" color="#fff" />
+export const TitleLarge = () => <Title testid="large-title" text="Titre large" size="large" color="#fff" />
+export const TitleMediumWithTextMirror = () => <Title testid="medium-textmiror-title" text="Titre medium miror" size="medium" color="#fff" textMirror="Titre medium" />
+export const TitleLargeWithTextMirror = () => <Title testid="large-textmiror-title" text="Titre large miror" size="large" color="#fff" textMirror="Titre large" />
