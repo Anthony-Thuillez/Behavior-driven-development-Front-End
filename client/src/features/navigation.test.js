ac72458e07d5968ba54f5b86630bedefadc8656c
Simon <soleausimon@hotmail.fr>
2020-04-29 18:01:50 +0200

    [TEST] navigation

---
+++ client/src/features/navigation.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components';
+
+import { Default } from '../stories/navigation.stories';
+
+describe('Renders Navigation component', () => {
+    let spy;
+
+    beforeEach(() => {
+        spy = jest.fn();
+    });
+
+    test('with a default state', () => {
+        const { container, getByTestId } = render(<Default onClick={spy} />);
+        expect(container).toBeTruthy();
+        const button = getByTestId('navigation-test')
+        button.click();
+        expect(spy).toHaveBeenCalledTimes(0);
+    });
+
+});
--- client/src/stories/navigation.stories.js
+++ client/src/stories/navigation.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import Navigation from '../components/molecules/navigation';
+import { BrowserRouter as Router } from "react-router-dom";
 
-export const Default = () => <Navigation testid="navigation-test" />;
+export const Default = () => <Router><Navigation testid="navigation-test" /></Router>;
